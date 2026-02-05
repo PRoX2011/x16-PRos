6065e4271dcda0176d06de14dd78872536221cf7
desvor58 <timmit445@mail.ru>
2026-02-05 15:48:58 +0500

    command history bugfix

--- src/kernel/features/string.asm
+++ src/kernel/features/string.asm
@@ -305,8 +305,6 @@ string_input_string:
     jmp .handle_history_scroll_up_loop
 
 .handle_history_scroll_up_done:
-    ; call string_get_cursor_pos
-    ; mov word [.cursor_col], dx
     jmp .read_loop
 
 
@@ -349,8 +347,6 @@ string_input_string:
     jmp .handle_history_scroll_down_loop
 
 .handle_history_scroll_down_done:
-    ; call string_get_cursor_pos
-    ; mov word [.cursor_col], dx
     jmp .read_loop
 
 .handle_history_scroll_down_history_exit:
@@ -374,8 +370,6 @@ string_input_string:
     jmp .handle_history_scroll_down_clear_loop_exit
 
 .handle_history_scroll_down_clear_loop_done:
-    ; call string_get_cursor_pos
-    ; mov word [.cursor_col], dx
     jmp .read_loop
 
 .handle_backspace:
